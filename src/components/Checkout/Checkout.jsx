ce25aca0b52241dc0d0e66fad31e1848893175c4
BaherDagher <baherosama5828@gmail.com>
2024-08-26 14:15:52 +0300

    homepageWithoutHas22h

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -43,9 +43,9 @@ export default function Checkout() {
 
   async function handleCheckout(values) {
     setisLoading(true);
-    const baseUrl = "https://baherdagher.github.io/Dagher-Market/redirect.html";
+    const redirectUrl = "https://baherdagher.github.io/Dagher-Market/allorders.html";
     const url =
-      isOnline ? `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?redirect=${encodeURIComponent(baseUrl + '?route=allorders')}`
+      isOnline ? `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=${encodeURIComponent(redirectUrl)}`
         : `https://ecommerce.routemisr.com/api/v1/orders/${cartId}`
 
 
